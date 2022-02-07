c6b06efaaaf2a454a8015babafaafdd89ba75d73
ThiagoSilva20 <thiago201714@gmail.com>
2022-02-07 17:15:23 -0300

    ajustando bd e heroku

--- api_todo/api_todo/settings.py
+++ api_todo/api_todo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,14 +77,8 @@ WSGI_APPLICATION = 'api_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
+DATABASES = {'default': dj_database_url.config(
+    conn_max_age=600, ssl_require=True)}
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
