46042f791af5d0ecdf810efa5d2ce177915ddc44
ThiagoSilva20 <thiago201714@gmail.com>
2022-02-08 17:42:14 -0300

    criou banco

--- api_todo/app/urls.py
+++ api_todo/app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from app import views
-from models import Usuario, Compras, Produto, Loja
 
 
 router = DefaultRouter()
