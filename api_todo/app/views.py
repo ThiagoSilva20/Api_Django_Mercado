0c99e59a442baf311c3f3d3bd2a3e74025807a90
ThiagoSilva20 <thiago201714@gmail.com>
2022-02-08 11:32:12 -0300

    diminuindo codigo no views

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -1,10 +1,10 @@
 from .models import Todo
-from urllib import response
 from .serializers import TodoSerializer
 
 ##############################################
 
 from rest_framework import status
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
